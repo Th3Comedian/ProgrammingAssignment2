b85a0e2a0dc0a557bd8ee53f08661650339473af
Th3Comedian <andrescorma@gmail.com>
2020-11-15 18:58:01 -0300

    First try at the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## My function will do pretty much the same thing the example function for the 
+## mean would do and has a similar structure.
+## set initializes the matrix, get returns the value of the matrix, set will  
+##assign the matrix new values and get will return the values of the inverse 
+##matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invm <- NULL
+        set <- function(y){
+                x <<- y
+                invm <<- NULL
+        }
+        get <- function(){
+                x
+        }
+        SetImat <- function(invmat){
+                invm <<- invmat
+        }
+        GetImat <- function(){
+                invm
+        }
+        list(set = set, get = get, SetImat=SetImat, GetImat=GetImat)
 }
 
 
-## Write a short comment describing this function
+## This function just as the example will first check whether the 
+##inverse matrix has already been filled and will return those values,
+#if its empty, it will compute the inverse matrix and store it using the 
+##SetImat function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invm <- x$GetImat()
+        if(!is.null(invm)){
+                message('getting cached data')
+                return(invm)
+        }
+        else{
+        data <- x$get()
+        invm <- solve(data, ...)
+        x$SetImat(invm)
+        invm
+        }
 }
